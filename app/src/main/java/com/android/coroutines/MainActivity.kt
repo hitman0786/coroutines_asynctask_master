370dd930f52f1b91f24843dec1031b1949400834
Dharmendra.Pratap <dharmendra.pratap@accedo.tv>
2021-05-13 18:15:29 +0530

    first commit

---
+++ app/src/main/java/com/android/coroutines/MainActivity.kt
@@ -0,0 +1,59 @@
+package com.android.coroutines
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.android.coroutines.lib.CoroutineAsyncTask
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    var count = 1
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btn.text = "start"
+        progressBar.max = 10
+
+        btn.setOnClickListener {
+            count =1;
+            progressBar.visibility = View.VISIBLE
+            progressBar.progress = 0
+
+            MyTask().executeAndReturn(10 as Integer)
+        }
+    }
+
+    inner class MyTask: CoroutineAsyncTask<Integer, Integer, String>(){
+        override fun onPreExecute() {
+            super.onPreExecute()
+            output.text = "Task Starting...";
+        }
+        override fun doInBackground(params: Array<out Integer>): String {
+            while (count <= params[0].toInt()) {
+                try {
+                    Thread.sleep(1000)
+                    publishProgress(count as Integer)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
+                }
+                count++
+            }
+            return "Task Completed."
+        }
+
+        @SuppressLint("SetTextI18n")
+        override fun onProgressUpdate(result: Array<out Any?>) {
+            super.onProgressUpdate(result)
+            output.text = "Running..."+ result[0] as Int
+            progressBar.progress = result[0] as Int
+        }
+
+        override fun onPostExecute(result: String) {
+            super.onPostExecute(result)
+            progressBar.visibility = View.GONE
+            output.text = result
+            btn.text = "Restart"
+        }
+    }
+}
---
+++ app/src/main/java/com/android/coroutines/lib/CoroutineAsyncTask.kt
@@ -0,0 +1,136 @@
+package com.android.coroutines.lib
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
+
+abstract class CoroutineAsyncTask<Params, Progress, Result> : CoroutineScope {
+    private val TAG = "CoroutineAsyncTask"
+    private var sHandler: InternalHandler? = null
+    private val MESSAGE_POST_PROGRESS = 1
+
+    override val coroutineContext: CoroutineContext
+        get() = Dispatchers.Main + job + handler
+
+    private val handler = CoroutineExceptionHandler { _, exception ->
+        Log.i(TAG, "$TAG $exception")
+    }
+
+    private val job: Job by lazy {
+        Job()
+    }
+
+    fun executeAndReturn(vararg params: Params): CoroutineAsyncTask<Params, Progress, Result> {
+        return executeCoroutineTask(params)
+    }
+
+    private fun executeCoroutineTask(params: Array<out Params>): CoroutineAsyncTask<Params, Progress, Result> {
+        onPreExecute()
+        val deferredResult = CoroutineScope(Dispatchers.IO).async {
+            doInBackground(params)
+        }
+        Log.i(TAG, "$TAG start executing result")
+        callOnPostExecute(deferredResult)
+        return this
+    }
+
+    private fun callOnPostExecute(deferredResult: Deferred<Result>){
+        launch {
+            try {
+                Log.i(TAG, "$TAG start launch")
+                val result = deferredResult.await()
+                onPostExecute(result)
+            } catch (exception: CancellationException) {
+                Log.i(TAG, "$TAG ignoring the cancellation exception")
+                onCancelled(exception)
+            } catch (exception: Exception) {
+                onCancelled(exception)
+            }
+        }
+    }
+
+    @WorkerThread
+    protected abstract fun doInBackground(params: Array<out Params>): Result
+
+    @MainThread
+    protected open fun onPreExecute() {
+        Log.i(TAG, "$TAG onPreExecute invoke")
+    }
+
+    @MainThread
+    protected open fun onPostExecute(result: Result) {
+        Log.i(TAG, "$TAG onPostExecute invoke")
+    }
+
+    @MainThread
+    protected open fun onProgressUpdate(values: Array<out Any?>) {
+        Log.i(TAG, "$TAG onProgressUpdate invoke")
+    }
+
+    @MainThread
+    protected open fun onCancelled() {
+        Log.i(TAG, "$TAG onCancelled invoke")
+        cancel()
+    }
+
+    @MainThread
+    protected open fun onCancelled(exception: Exception?) {
+        Log.i(TAG, "$TAG onCancelled with exception invoke")
+        onCancelled()
+    }
+
+    fun cancel() {
+        if (!job.isCancelled && job.isActive) {
+            job.cancel()
+            Log.i(TAG, "$TAG job has been canceled!!!!!")
+        }
+    }
+
+    fun isCancelled(): Boolean {
+        return job.isCancelled && !job.isActive
+    }
+
+    private fun getMainHandler(): Handler? {
+        synchronized(CoroutineAsyncTask::class.java) {
+            if (sHandler == null) {
+                sHandler = InternalHandler(Looper.getMainLooper())
+            }
+            return sHandler
+        }
+    }
+
+    private fun getHelperHandler(): Handler {
+        return getMainHandler() ?: InternalHandler(Looper.getMainLooper())
+    }
+
+
+    @WorkerThread
+    protected fun publishProgress(vararg values: Progress) {
+        if (!isCancelled()) {
+            getHelperHandler().obtainMessage(
+                MESSAGE_POST_PROGRESS,
+                AsyncTaskResult(this, *values)
+            ).sendToTarget()
+        }
+    }
+
+    private inner class InternalHandler(looper: Looper) : Handler(looper) {
+        override fun handleMessage(msg: Message) {
+            val result: AsyncTaskResult<*> = msg.obj as AsyncTaskResult<*>
+            Log.i(TAG, "$TAG progress update : ${result.mData}")
+            result.mTask.onProgressUpdate(result.mData)
+        }
+    }
+
+    private class AsyncTaskResult<Data>(
+        val mTask: CoroutineAsyncTask<*, *, *>,
+        vararg data: Data
+    ) {
+        val mData: Array<Data> = data as Array<Data>
+    }
+}
